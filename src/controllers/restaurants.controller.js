fd88c3c71d583570e89fbef224e097031560564d
Alejandro Rodriguez <alejandrorv944@gmail.com>
2021-10-11 22:03:23 -0500

    CRUD completed

--- src/controllers/restaurants.controller.js
+++ src/controllers/restaurants.controller.js
@@ -6,12 +6,84 @@ const getRestaurants = async (req, res) => {
         const query = 'SELECT * FROM restaurants';
 
         const data = await pool.query(query);
-        console.log(data.rows)
-        res.send('ok')
+        
+        res.status(200).json(data.rows);
     }
     catch(e){
+        res.status(400).json({msg:'Something went wrong'})
+    }
+}
+const getRestaurant = async (req, res) => {
+    try{
+        const {id} = req.params
+        
+        const query = `SELECT * FROM restaurants WHERE id='${id}'`;
 
+        const data = await pool.query(query);
+        
+        res.status(200).json(data.rows);
+    }
+    catch(e){
+        res.status(400).json({msg:'Something went wrong'})
+    }
+}
+const createRestaurant = async (req, res) => {
+    try{
+
+        const {id,rating ,name ,site ,email ,phone ,street ,city ,state ,lat, lng } = req.body;
+        
+        const query = `INSERT INTO restaurants VALUES('${id}',${rating},'${name}','${site}','${email}','${phone}','${street}','${city}','${state}',${lat},${lng})`;
+
+        const data = await pool.query(query);
+        
+        res.status(201).json({msg:'User created successfully', data});
+
+    }
+    catch(e){
+        res.status(400).json({msg:'Something went wrong'})
+    }
+}
+const updateRestaurant = async (req, res) => {
+    
+    try{
+        const {id} = req.params;
+        
+        let query = `SELECT * FROM restaurants WHERE id='${id}'`;
+
+        let data = await pool.query(query);
+
+        if (data.rowCount === 0) return res.status(404).json({msg:'Restaurant not found'});
+
+        const {rating ,name ,site ,email ,phone ,street ,city ,state ,lat, lng } = req.body;
+
+        query = `UPDATE restaurants SET rating=${rating}, name='${name}',site='${site}',email='${email}',phone='${phone}',street='${street}',city='${city}',state='${state}',lat=${lat},lng=${lng} WHERE id='${id}'`;
+        
+        data = await pool.query(query);
+        
+        res.status(200).json({msg:'Restaurant updated successfully'});
+    }
+    catch(e){
+        console.log(e.toString())
+        res.status(400).json({msg:'Something went wrong'})
+    }
+}
+const deleteRestaurant = async (req, res) => {
+    
+    try{
+        const {id} = req.params;
+        const query = `DELETE FROM restaurants WHERE id='${id}'`;
+        await pool.query(query)
+        res.status(200).json({msg:`Restaurant with id ${id} deleted successfully`});
+    }
+    catch(e){
+        res.status(400).json({msg:'Something went wrong'})
     }
 }
 
-module.exports = {getRestaurants}
+module.exports = {
+    getRestaurants,
+    getRestaurant,
+    createRestaurant,
+    updateRestaurant,
+    deleteRestaurant
+}
--- src/routes/restaurants.routes.js
+++ src/routes/restaurants.routes.js
@@ -1,9 +1,13 @@
 const {Router} = require('express');
 const router = Router();
 
-const {getRestaurants} = require('../controllers/restaurants.controller');
+const {getRestaurants, getRestaurant, createRestaurant, updateRestaurant, deleteRestaurant} = require('../controllers/restaurants.controller');
 
 router.get('/', getRestaurants)
+router.get('/:id', getRestaurant)
+router.post('/', createRestaurant)
+router.put('/:id', updateRestaurant)
+router.delete('/:id', deleteRestaurant)
 
 
 module.exports = router;
